35c1cc7808602aa7be4b382cb573d6ec269fb3d4
PeterSlow <dcc_slow@hotmail.com>
2023-02-07 21:32:44 +0100

    Header and footer
    Realización del nav, el header y el footer

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 function obtenerLocalizacionActual() {
-    let latitude;
-    let longitude;
-  
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-  
-        let ciudad = getCityName(latitude, longitude);
-  
-        console.log(ciudad);
-      });
-    } else {
-      console.error("La geolocalizacion no está disponible en su navegador");
-    }
-  
-    function getCityName(lat, lng) {
-      const API_URL = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`;
-  
-      fetch(API_URL)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data.address.town || data.address.city);
-        });
-    }
+  let latitude;
+  let longitude;
+
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+
+      let ciudad = getCityName(latitude, longitude);
+
+      console.log(ciudad);
+    });
+  } else {
+    console.error("La geolocalizacion no está disponible en su navegador");
   }
-  
-  obtenerLocalizacionActual();
+
+  function getCityName(lat, lng) {
+    const API_URL = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`;
+
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.address.town || data.address.city);
+      });
+  }
+}
+
+obtenerLocalizacionActual();
+
+// Cuando el usuario se desplaza hacia abajo 80 px desde la parte superior del documento, cambie el tamaño del relleno de la barra de navegación y el tamaño de fuente del logotipo
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
