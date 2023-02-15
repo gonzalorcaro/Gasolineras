7d76276f3c7bd0fa18150a98821547c3ad25d7bd
PeterSlow <dcc_slow@hotmail.com>
2023-02-15 19:06:06 +0100

    d

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
+
+//API KEY googlemaps: AIzaSyBWZJ5Yig-1yOrgN4XtEuIzGtuWhIr4Bgs
+
 let idMunicipio;
-let divGasolineras = document.getElementById("gasolineras");
 
 gasolinerasInicio();
 
@@ -13,16 +15,8 @@ async function gasolinerasInicio() {
     coordenadas.longitud
   );
   let idMunicipio = await obtenerIdMunicipio(ciudad);
-  let idGasoil = 4;
-  let datosGasolineras = await gasolineasPorProductoYMunicipio(idMunicipio, idGasoil);
-
-  let datosGasolinerasOrdenados = ordenarPorDistancia(datosGasolineras.ListaEESSPrecio, coordenadas.latitud, coordenadas.longitud);
-  console.log(datosGasolineras);
-
-  datosGasolinerasOrdenados.forEach((gasolinera) => {
-    mostrarGasolinera(gasolinera, "Gasóleo A");
-  });
-
+  let idProducto = 1;
+  gasolineasPorProductoYMunicipio(idProducto, idMunicipio);
 }
 
 // funcion que obtiene la ip publica del cliente
@@ -89,121 +83,25 @@ function obtenerLocalizacionActual() {
   }
 }
 
-// funcion para obtener las gasolineras de un municipio.
-async function gasolineasPorMunicipio(idMunicipio) {
-  let response;
-  let API_URL = `https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroMunicipio/${idMunicipio}`;
-  response = await fetch(API_URL);
-
-  let datosGasolineras = await response.json();
-
-  return datosGasolineras;
-}
-
 // funcion para obtener las gasolineras de ese idProducto e idMunicipio.
-async function gasolineasPorProductoYMunicipio(idMunicipio, idProducto) {
+async function gasolineasPorProductoYMunicipio(idProducto, idMunicipio) {
   let response;
-  let API_URL = `https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroMunicipioProducto/${idMunicipio}/${idProducto}`;
+  let API_URL = `https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroMunicipioProducto/${idProducto}/${idMunicipio}`;
   response = await fetch(API_URL);
 
   let datosGasolineras = await response.json();
-  return datosGasolineras;
-}
-
-function ordenarPorDistancia(gasolineras, latRef, lonRef) {
-  // Agregar distancia a cada coordenada
-  const gasolinerasConDistancia = gasolineras.map((gasolinera) => {
-    let lat = parseFloat(gasolinera.Latitud.replace(",", "."));
-    let lon = parseFloat(gasolinera["Longitud (WGS84)"].replace(",", "."));
-    const distancia = calcularDistancia(lat, lon, latRef, lonRef);
-    return { gasolinera, distancia };
-  });
-
-  // Ordenar coordenadas por distancia ascendente
-  gasolinerasConDistancia.sort((a, b) => a.distancia - b.distancia);
-
-  return gasolinerasConDistancia;
-}
-
-// funcion que toma las coordenadas de dos puntos (latitud y longitud) y devuelve la distancia en kilómetros entre ellos
-function calcularDistancia(lat1, lon1, lat2, lon2) {
-
-  const radioTierra = 6371; // Radio de la Tierra en kilómetros
-  const dLat = toRad(lat2 - lat1);
-  const dLon = toRad(lon2 - lon1);
-  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-    Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distancia = radioTierra * c; // Distancia en kilómetros
-  return distancia;
-}
-
-// La función toRad se utiliza para convertir grados a radianes
-function toRad(grados) {
-  return grados * Math.PI / 180;
-}
-
-
-// pintar una gasolinera
-function mostrarGasolinera(gasolinera, nombreCarburante) {
-
-  let divGasolinera = document.createElement("div");
-  divGasolinera.classList.add("gasolinera");
-
-  let divFoto = document.createElement("div");
-  divFoto.classList.add("divFoto");
-  divGasolinera.appendChild(divFoto);
-
-  let nombreLogo = obtenerLogoGas(gasolinera.gasolinera.Rótulo);
-
-  let logo = document.createElement("img");
-  logo.classList.add("logo");
-  logo.src = `img/logos/${nombreLogo}.png`;
-
-  divFoto.appendChild(logo);
-
-  let divContenidoGas = document.createElement("div");
-  divContenidoGas.classList.add("divContenidoGas");
-
-  let pDireccion = document.createElement("p");
-  pDireccion.innerHTML = "<b>" + gasolinera.gasolinera.Dirección + "</b>";
-  divContenidoGas.appendChild(pDireccion);
-
-  let pPrecioProducto = document.createElement("p");
-  pPrecioProducto.classList.add("pPrecioProducto");
-  pPrecioProducto.innerHTML = nombreCarburante + ": " + gasolinera.gasolinera.PrecioProducto + "€";
-  divContenidoGas.appendChild(pPrecioProducto);
-
-  let distancia = document.createElement("p");
-  distancia.id = "distanciaGas";
-  distancia.innerHTML = gasolinera.distancia.toFixed(2) + " km";
-  divContenidoGas.appendChild(distancia);
-
-  divGasolinera.appendChild(divContenidoGas);
-  divGasolineras.appendChild(divGasolinera);
-}
-
-// funcion para
-function obtenerLogoGas(nombre) {
-  let nombreEnMinusculas = nombre.toLowerCase();
-  let nombreFoto;
-  switch (nombreEnMinusculas) {
-    case "cepsa":
-      nombreFoto = "cepsa";
-      break;
-    case "repsol":
-      nombreFoto = "repsol";
-      break;
-    case "shell":
-      nombreFoto = "shell";
-      break;
-    default:
-      nombreFoto = "defaultFoto";
-      break;
+  console.log(datosGasolineras);
+  if (datosGasolineras.ListaEESSPrecio.length != 0) {
+    datosGasolineras.ListaEESSPrecio.forEach((gasolinera) => {
+      mostrarGasolinera(gasolinera);
+    });
+  } else {
+    noHayResultados();
   }
-
-  return nombreFoto;
+}
+// pintar una gasolinera
+function mostrarGasolinera(gasolinera) {
+  console.log(gasolinera);
 }
 
 // función para cuando un filtro no obtenga resultados.
@@ -213,10 +111,7 @@ function noHayResultados() {
 
 
 
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 //'apikey': 'ucstkoCXcmlx8N1_6KdtT2akr6IoR7ja57jFoU0Fgro'
 //Mapa
 // Crea una instancia del servicio de plataforma HERE Maps:
@@ -243,24 +138,13 @@ var ui = H.ui.UI.createDefault(map, defaultLayers);
 var searchService = platform.getSearchService();
 
 // Crea una marca en el mapa en las coordenadas específicas
-<<<<<<< Updated upstream
-funciton marcarMapa(lat, long){
-  var marker = new H.map.Marker({
-    lat: lat,
-    lng: long
-  });
-  // Agrega la marca al mapa
-  map.addObject(marker);
-}
-
-
-=======
 var marker = new H.map.Marker({
   lat: 38.9161100,
   lng: -6.3436600
 });
->>>>>>> Stashed changes
 
+// Agrega la marca al mapa
+map.addObject(marker);
 
 
 
